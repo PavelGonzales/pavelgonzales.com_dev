ece8b9249456444593c91658abc3b37d7b43d48f
Pavel Gonzales <develop.gonzales@gmail.com>
2017-06-04 14:55:48 +0300

    сделал динамический роутинг и поправил ссылки на страницы

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   #app
     .main-view
       .nav
-        router-link.nav__arrow.up(to="resume") резюме
-        router-link.nav__arrow.left(to="portfolio") портфолио
-        router-link.nav__arrow.right(to="contacts") контакты
-        router-link.nav__arrow.bottom(to="/") главная
+        router-link.nav__arrow.up(:to="topLink") {{ topLinkText }}
+        router-link.nav__arrow.left(:to="leftLink") {{ leftLinkText }}
+        router-link.nav__arrow.right(:to="rightLink") {{ rightLinkText }}
+        router-link.nav__arrow.bottom(:to="bottomLink") {{ bottomLinkText }}
       router-view.router-view
 </template>
 
@@ -19,26 +19,74 @@ export default {
     if (this.$route.name === 'Main') {
       this.tl.fromTo(this.mainView, 0.6, { scale: 1.5, rotationX: 90 },
                                          { scale: 1, rotationX: 0 });
+      this.topLinkText = 'резюме';
+      this.leftLinkText = 'портфолио';
+      this.rightLinkText = 'контакты';
+      this.bottomLinkText = 'блог';
+      this.topLink = 'resume';
+      this.leftLink = 'portfolio';
+      this.rightLink = 'contacts';
+      this.bottomLink = 'blog';
     }
     if (this.$route.name === 'Resume') {
       this.tl.fromTo(this.mainView, 0.6, { scale: 1.5, rotationX: 90 },
                                           { scale: 1, rotationX: 0 });
+      this.topLinkText = 'главная';
+      this.leftLinkText = 'портфолио';
+      this.rightLinkText = 'контакты';
+      this.bottomLinkText = 'блог';
+      this.topLink = '/';
+      this.leftLink = 'portfolio';
+      this.rightLink = 'contacts';
+      this.bottomLink = 'blog';
     }
     if (this.$route.name === 'Blog') {
       this.tl.fromTo(this.mainView, 0.6, { scale: 1.5, rotationX: -90 },
                                           { scale: 1, rotationX: 0 });
+      this.topLinkText = 'резюме';
+      this.leftLinkText = 'портфолио';
+      this.rightLinkText = 'контакты';
+      this.bottomLinkText = 'главная';
+      this.topLink = 'resume';
+      this.leftLink = 'portfolio';
+      this.rightLink = 'contacts';
+      this.bottomLink = '/';
     }
     if (this.$route.name === 'Portfolio') {
       this.tl.fromTo(this.mainView, 0.6, { scale: 1.5, rotationY: 90 },
                                           { scale: 1, rotationY: 0 });
+      this.topLinkText = 'резюме';
+      this.leftLinkText = 'главная';
+      this.rightLinkText = 'контакты';
+      this.bottomLinkText = 'блог';
+      this.topLink = 'resume';
+      this.leftLink = '/';
+      this.rightLink = 'contacts';
+      this.bottomLink = 'blog';
     }
     if (this.$route.name === 'Contacts') {
       this.tl.fromTo(this.mainView, 0.6, { scale: 1.5, rotationY: -90 },
                                           { scale: 1, rotationY: 0 });
+      this.topLinkText = 'резюме';
+      this.leftLinkText = 'портфолио';
+      this.rightLinkText = 'главная';
+      this.bottomLinkText = 'блог';
+      this.topLink = 'resume';
+      this.leftLink = 'portfolio';
+      this.rightLink = '/';
+      this.bottomLink = 'blog';
     }
   },
   data: () => ({
     tl: new TimelineMax(),
+    topLinkText: '',
+    leftLinkText: '',
+    rightLinkText: '',
+    bottomLinkText: '',
+    topLink: '',
+    leftLink: '',
+    rightLink: '',
+    bottomLink: '',
   }),
   watch: {
     $route(to, from) {
@@ -48,6 +96,14 @@ export default {
           (to.name === 'Resume' && from.name === 'Contacts')) {
         this.tl.fromTo(this.mainView, 1, { rotationY: 0, rotationX: -180 },
                                          { rotationY: 0, rotationX: 0 });
+        this.topLinkText = 'главная';
+        this.leftLinkText = 'портфолио';
+        this.rightLinkText = 'контакты';
+        this.bottomLinkText = 'блог';
+        this.topLink = '/';
+        this.leftLink = 'portfolio';
+        this.rightLink = 'contacts';
+        this.bottomLink = 'blog';
       }
       if ((to.name === 'Portfolio' && from.name === 'Main') ||
           (to.name === 'Portfolio' && from.name === 'Blog') ||
@@ -55,6 +111,14 @@ export default {
           (to.name === 'Portfolio' && from.name === 'Contacts')) {
         this.tl.fromTo(this.mainView, 1, { rotationX: 0, rotationY: -180 },
                                          { rotationX: 0, rotationY: 0 });
+        this.topLinkText = 'резюме';
+        this.leftLinkText = 'главная';
+        this.rightLinkText = 'контакты';
+        this.bottomLinkText = 'блог';
+        this.topLink = 'resume';
+        this.leftLink = '/';
+        this.rightLink = 'contacts';
+        this.bottomLink = 'blog';
       }
       if ((to.name === 'Blog' && from.name === 'Main') ||
           (to.name === 'Blog' && from.name === 'Resume') ||
@@ -62,6 +126,14 @@ export default {
           (to.name === 'Blog' && from.name === 'Contacts')) {
         this.tl.fromTo(this.mainView, 1, { rotationY: 0, rotationX: 180 },
                                      { rotationY: 0, rotationX: 0 });
+        this.topLinkText = 'резюме';
+        this.leftLinkText = 'портфолио';
+        this.rightLinkText = 'контакты';
+        this.bottomLinkText = 'главня';
+        this.topLink = 'resume';
+        this.leftLink = 'portfolio';
+        this.rightLink = 'contacts';
+        this.bottomLink = '/';
       }
       if ((to.name === 'Contacts' && from.name === 'Main') ||
           (to.name === 'Contacts' && from.name === 'Resume') ||
@@ -69,18 +141,58 @@ export default {
           (to.name === 'Contacts' && from.name === 'Blog')) {
         this.tl.fromTo(this.mainView, 1, { rotationX: 0, rotationY: 180 },
                                      { rotationX: 0, rotationY: 0 });
+        this.topLinkText = 'резюме';
+        this.leftLinkText = 'портфолио';
+        this.rightLinkText = 'главная';
+        this.bottomLinkText = 'блог';
+        this.topLink = 'resume';
+        this.leftLink = 'portfolio';
+        this.rightLink = '/';
+        this.bottomLink = 'blog';
       }
       if (to.name === 'Main' && from.name === 'Resume') {
         this.tl.fromTo(this.mainView, 1, { rotationX: -180 }, { rotationX: 0 });
+        this.topLinkText = 'резюме';
+        this.leftLinkText = 'портфолио';
+        this.rightLinkText = 'контакты';
+        this.bottomLinkText = 'блог';
+        this.topLink = 'resume';
+        this.leftLink = 'portfolio';
+        this.rightLink = 'contacts';
+        this.bottomLink = 'blog';
       }
       if (to.name === 'Main' && from.name === 'Portfolio') {
         this.tl.fromTo(this.mainView, 1, { rotationY: -180 }, { rotationY: 0 });
+        this.topLinkText = 'резюме';
+        this.leftLinkText = 'портфолио';
+        this.rightLinkText = 'контакты';
+        this.bottomLinkText = 'блог';
+        this.topLink = 'resume';
+        this.leftLink = 'portfolio';
+        this.rightLink = 'contacts';
+        this.bottomLink = 'blog';
       }
       if (to.name === 'Main' && from.name === 'Blog') {
         this.tl.fromTo(this.mainView, 1, { rotationX: 180 }, { rotationX: 0 });
+        this.topLinkText = 'резюме';
+        this.leftLinkText = 'портфолио';
+        this.rightLinkText = 'контакты';
+        this.bottomLinkText = 'блог';
+        this.topLink = 'resume';
+        this.leftLink = 'portfolio';
+        this.rightLink = 'contacts';
+        this.bottomLink = 'blog';
       }
       if (to.name === 'Main' && from.name === 'Contacts') {
         this.tl.fromTo(this.mainView, 1, { rotationY: 180 }, { rotationY: 0 });
+        this.topLinkText = 'резюме';
+        this.leftLinkText = 'портфолио';
+        this.rightLinkText = 'контакты';
+        this.bottomLinkText = 'блог';
+        this.topLink = 'resume';
+        this.leftLink = 'portfolio';
+        this.rightLink = 'contacts';
+        this.bottomLink = 'blog';
       }
       console.log('beforeRouteUpdate', to, from);
     },
@@ -120,7 +232,7 @@ body
     z-index -1
 
 #app
-  transform perspective(800px)
+  transform perspective(10000px)
   transform-style preserve-3d
   font-family 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
@@ -178,17 +290,23 @@ body
     color #fff
 
     &.up
+      transform translateX(-50%)      
       top 0 
       left 50%
     &.bottom 
+      transform translateX(-50%)      
       bottom 0
       left 50%
     &.left 
+      transform-origin right top      
+      transform rotate(90deg) translateX(50%)
       top 50%
       right 0
     &.right 
-      left 0
+      transform-origin left top      
+      transform rotate(-90deg) translateX(-50%)
       top 50%
+      left 0
 
 .router-view
   position absolute 
